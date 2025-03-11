197211d7bdde9e8dec496528583349a6627e968c
Cristiano Cruz <cristiano@linvix.com.br>
2025-03-11 15:04:31 -0300

    new: export Possible UFs typing

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { mapping } from "./core";
 import { PossibleStates } from "./types";
 
+export * from "./types";
+
 export const validateIE = (uf: PossibleStates, ie: string) => {
     if (!mapping[uf]) throw new Error(`A UF ${uf} não foi encontrada`);
     return mapping[uf](ie);
